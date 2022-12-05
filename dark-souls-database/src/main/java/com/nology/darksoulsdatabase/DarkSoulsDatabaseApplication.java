a3ecf70845b531cad25a7d8a0ec3bcc912271015
Lewis Lovett <llovett7575@gmail.com>
2022-12-05 14:46:44 +0000

    Spring Boot Initalised

---
+++ dark-souls-database/src/main/java/com/nology/darksoulsdatabase/DarkSoulsDatabaseApplication.java
@@ -0,0 +1,14 @@
+package com.nology.darksoulsdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class DarkSoulsDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarkSoulsDatabaseApplication.class, args);
+	}
+
+}
---
+++ dark-souls-database/src/test/java/com/nology/darksoulsdatabase/DarkSoulsDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nology.darksoulsdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkSoulsDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
