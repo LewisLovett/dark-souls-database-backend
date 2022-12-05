112a7635009e61a1e262814434477ad191b7de02
Lewis Lovett <llovett7575@gmail.com>
2022-12-05 15:15:26 +0000

    Created Boss model

---
+++ dark-souls-database/src/main/java/com/nology/darksoulsdatabase/Boss.java
@@ -0,0 +1,79 @@
+package com.nology.darksoulsdatabase;
+
+import javax.persistence.*;
+
+@Entity
+
+public class Boss {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String bossName;
+    private int bossHealth;
+    private int bossSouls;
+    private String bossImage;
+    private String bossDesc;
+
+    public Boss(){
+
+    }
+
+    public Boss(int id, String bossName, int bossHealth, int bossSouls, String bossImage, String bossDesc) {
+        this.id = id;
+        this.bossName = bossName;
+        this.bossHealth = bossHealth;
+        this.bossSouls = bossSouls;
+        this.bossImage = bossImage;
+        this.bossDesc = bossDesc;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBossName() {
+        return bossName;
+    }
+
+    public void setBossName(String bossName) {
+        this.bossName = bossName;
+    }
+
+    public int getBossHealth() {
+        return bossHealth;
+    }
+
+    public void setBossHealth(int bossHealth) {
+        this.bossHealth = bossHealth;
+    }
+
+    public int getBossSouls() {
+        return bossSouls;
+    }
+
+    public void setBossSouls(int bossSouls) {
+        this.bossSouls = bossSouls;
+    }
+
+    public String getBossImage() {
+        return bossImage;
+    }
+
+    public void setBossImage(String bossImage) {
+        this.bossImage = bossImage;
+    }
+
+    public String getBossDesc() {
+        return bossDesc;
+    }
+
+    public void setBossDesc(String bossDesc) {
+        this.bossDesc = bossDesc;
+    }
+}
